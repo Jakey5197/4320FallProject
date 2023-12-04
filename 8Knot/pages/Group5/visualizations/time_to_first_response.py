9fe864e38dc0579d1c061a7399a82dfa8d9f9e9a
chris3309 <christopher@putzler.org>
2023-12-03 19:51:58 -0600

    time to first response
    created time to first response graph

--- 8Knot/pages/Group5/visualizations/time_to_first_response.py
+++ 8Knot/pages/Group5/visualizations/time_to_first_response.py
@@ -8,7 +8,7 @@ import logging
 from dateutil.relativedelta import *  # type: ignore
 import plotly.express as px
 from pages.utils.graph_utils import get_graph_time_values, color_seq
-from queries.ttfr_query import TTFRQ_query as ttfrq
+from queries.prs_query import prs_query as prq
 import io
 from cache_manager.cache_manager import CacheManager as cm
 from pages.utils.job_utils import nodata_graph
@@ -16,6 +16,10 @@ import time
 
 """
 NOTE: VARIABLES TO CHANGE:
+
+(5) CONTEXT OF GRAPH
+(6) IDs of Dash components
+(7) COLUMN_WITH_DATETIME
 (8) COLUMN_WITH_DATETIME
 (9) COLUMN_TO_SORT_BY
 (10) Comments before callbacks
@@ -50,7 +54,7 @@ time_to_first_response = dbc.Card(
         dbc.CardBody(
             [
                 html.H3(
-                    "Time To First Response",
+                    "Time to First Response",
                     className="card-title",
                     style={"textAlign": "center"},
                 ),
@@ -63,7 +67,7 @@ time_to_first_response = dbc.Card(
                                         how active a project comminuty currently is. Mesured here is response time across various activities
                                         including posts, emails, issues, and more. Longer response times indicates a community becoming inactive, 
                                         while a shorter response time indicates and facilitates further communication within a project."""
-                        ),
+                            ),
                     ],
                     id=f"popover-{PAGE}-{VIZ_ID}",
                     target=f"popover-target-{PAGE}-{VIZ_ID}",
@@ -145,13 +149,13 @@ def toggle_popover(n, is_open):
     ],
     background=True,
 )
-def time_to_first_response_graph(repolist, interval):
+def ttfr_graph(repolist, interval):
     # wait for data to asynchronously download and become available.
     cache = cm()
-    df = cache.grabm(func=ttfrq, repos=repolist)
+    df = cache.grabm(func=prq, repos=repolist)
     while df is None:
         time.sleep(1.0)
-        df = cache.grabm(func=ttfrq, repos=repolist)
+        df = cache.grabm(func=prq, repos=repolist)
 
     start = time.perf_counter()
     logging.warning(f"{VIZ_ID}- START")
@@ -162,91 +166,57 @@ def time_to_first_response_graph(repolist, interval):
         return nodata_graph
 
     # function for all data pre processing, COULD HAVE ADDITIONAL INPUTS AND OUTPUTS
-    df, df_contribs = process_data(df, interval)
+    df = process_data(df, interval)
 
-    fig = create_figure(df, df_contribs,interval)
+    fig = create_figure(df, interval)
 
     logging.warning(f"{VIZ_ID} - END - {time.perf_counter() - start}")
     return fig
 
 
 def process_data(df: pd.DataFrame, interval):
-    """Implement your custom data-processing logic in this function.
-    The output of this function is the data you intend to create a visualization with,
-    requiring no further processing."""
+    # Convert to datetime objects rather than strings
+    df["created"] = pd.to_datetime(df["created"], utc=True)
+    df["closed"] = pd.to_datetime(df["closed"], utc=True)
 
-    # convert to datetime objects rather than strings
-    # ADD ANY OTHER COLUMNS WITH DATETIME
-    df["created_at"] = pd.to_datetime(df["created_at"], utc=True)
-    df["closed_at"] = pd.to_datetime(df["closed_at"], utc=True)
+    # Order values chronologically by created date
+    df = df.sort_values(by="created", axis=0, ascending=True)
 
+    # Calculate the difference between closed and created dates
+    df["time_to_close"] = (df["closed"] - df["created"]).dt.days
 
-    """LOOK AT OTHER VISUALIZATIONS TO SEE IF ANY HAVE A SIMILAR DATA PROCESS"""
+    # Filter closed requests closed within 2 days of being opened
+    df_filtered = df[df["time_to_close"] <= 2]
 
-    created_range = pd.to_datetime(df["created"]).dt.to_period(interval).value_counts().sort_index()
+    return df_filtered
 
-    # converts to data frame object and creates date column from period values
-    df_contribs = created_range.to_frame().reset_index().rename(columns={"index": "Date", "created": "contribs"})
+def create_figure(df: pd.DataFrame, interval):
+    # Create a line graph with plotly
+    fig = go.Figure()
 
-    # converts date column to a datetime object, converts to string first to handle period information
-    df_contribs["Date"] = pd.to_datetime(df_contribs["Date"].astype(str))
+    # Add a trace for the total number of PRs opened
+    fig.add_trace(go.Scatter(
+        x=df["created"],
+        y=df.groupby(pd.Grouper(key="created", freq=interval))["created"].count(),
+        mode='lines',
+        name='Total PRs Opened'
+    ))
 
-    # correction for year binning -
-    # rounded up to next year so this is a simple patch
-    if interval == "Y":
-        df_contribs["Date"] = df_contribs["Date"].dt.year
-    elif interval == "M":
-        df_contribs["Date"] = df_contribs["Date"].dt.strftime("%Y-%m")
+    # Add a trace for the number of PRs closed within 2 days
+    fig.add_trace(go.Scatter(
+        x=df["closed"],
+        y=df.groupby(pd.Grouper(key="closed", freq=interval))["closed"].count(),
+        mode='lines',
+        name='PRs Closed Within 2 Days'
+    ))
 
-    return df, df_contribs
-
-
-def create_figure(df: pd.DataFrame, df_contribs, interval):
-    # time values for graph
-    x_r, x_name, hover, period = get_graph_time_values(interval)
-
-    if interval == -1:
-        fig = px.line(df, x="created_at", y=df.index, color_discrete_sequence=[color_seq[3]])
-        # fig.update_traces(hovertemplate="Contributors: %{y}<br>%{x|%b %d, %Y} <extra></extra>")
-    else:
-        fig = px.bar(
-            df_contribs,
-            x="Date",
-            y="Time to Respond",
-            range_x=x_r,
-            labels={"x": x_name, "y": "Contributors"},
-            color_discrete_sequence=[color_seq[3]],
-        )
-        fig.update_traces(hovertemplate=hover + "<br>Contributors: %{y}<br>")
-
-    """
-        Ref. for this awesome button thing:
-        https://plotly.com/python/range-slider/
-    """
-    # add the date-range selector
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=6, label="6m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
-                        dict(count=1, label="1y", step="year", stepmode="backward"),
-                        dict(step="all"),
-                    ]
-                )
-            ),
-            rangeslider=dict(visible=True),
-            type="date",
-        )
-    )
-    # label the figure correctly
+    # Set the layout
     fig.update_layout(
         xaxis_title="Time",
-        yaxis_title="Number of Contributors",
-        margin_b=40,
-        margin_r=20,
-        font=dict(size=14),
+        yaxis_title="# of PRs",
+        title=f"Time to First Response ({interval})",
+        hovermode="x"
     )
+
     return fig
+
